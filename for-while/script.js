dce0967bcc504d2a5ce8e39ec9b475b0c65aa0ff
Caio Nunes <caionunes2801@gmail.com>
2022-06-28 19:09:56 -0300

    second commit

---
+++ for-while/script.js
@@ -0,0 +1,37 @@
+// Criar um loop que conte de 1 até 10 usando FOR
+console.log("First Exercise")
+  for (let upCounter = 1; upCounter <= 10 ; upCounter++){
+    console.log(upCounter)
+  }
+
+// console.log("First Exercise2")
+// let upCounter2 = 1
+//   while(upCounter2 <= 10){
+//   console.log(upCounter2)
+//   upCounter2 = upCounter2 + 1 
+// }
+
+// Criar um loop que conte de 10 até 1 usando WHILE
+console.log("Second Exercise")
+  let downCounter = 10
+    while(downCounter >= 1){
+      console.log(downCounter)
+      downCounter = downCounter - 1
+    }
+
+//Criar um loop que conte todos os números ímpares de 1 até 100
+console.log("Third Exercise")
+  let firstLooping = 1
+    while (firstLooping <= 100){
+      console.log(firstLooping)
+      firstLooping = firstLooping + 2 
+    }
+
+//Criar um loop que conte todos os números pares de 0 a 100
+console.log("Fourth Exercise")
+  let secondLooping = 0
+    while (secondLooping <= 100){
+      console.log(secondLooping)
+      secondLooping = secondLooping + 2
+    }
+ 
