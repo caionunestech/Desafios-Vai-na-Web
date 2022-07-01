665fc41cf96fb10c5bfd728582bfa223ff7e6f34
Caio Nunes <caionunes2801@gmail.com>
2022-06-30 22:54:49 -0300

    fourth commit

---
+++ array/script.js
@@ -0,0 +1,24 @@
+let ingredients = [
+  'bakingSoda',
+  'milk',
+  'vanillaEssence',
+  'powderedSugar',
+  'butter',
+]
+console.log(ingredients)
+
+ingredients.unshift('eggs')
+console.log(ingredients)
+
+ingredients.pop()
+console.log(ingredients)
+
+ingredients.push('biologicalYeast','almondFlour')
+console.log(ingredients)
+
+ingredients.shift()
+console.log(ingredients)
+
+let ascendingOrder = [7,5,6,3,8,9,2,1,4]
+ascendingOrder.sort()
+console.log(ascendingOrder)
---
+++ structure-of-complex-objects/script.js
@@ -0,0 +1,44 @@
+let object = {
+  fatherName: 'Paulo',
+  motherName: 'Ana',
+  adressName: 'Rua Lavínia Magalhães'
+}
+object.houseNumber = 301
+console.log(object)
+
+delete object.adressName
+console.log(object)
+
+let registration = [{
+  firstName: 'Luis',
+  age: 20,
+  DDD: 021,   
+  friends: ('Tinky', 'Dipsy', 'Laa-Laa', 'Poo')
+},
+{
+  firstName: 'Luis',
+  age: 20,
+  DDD: 021,   
+  friends: ('Tinky', 'Dipsy', 'Laa-Laa', 'Poo')
+},
+{
+  firstName: 'Isabela',
+  age: 31,
+  DDD: 071,   
+  friends: ('Tinky', 'Dipsy', 'Laa-Laa', 'Poo')
+},
+{
+  firstName: 'Gabriele',
+  age: 22,
+  DDD: 075,   
+  friends: ('Tinky', 'Dipsy', 'Laa-Laa', 'Poo')
+},
+{
+  firstName: 'Carlos',
+  age: 25,
+  DDD: 011,   
+  friends: ('Tinky', 'Dipsy', 'Laa-Laa', 'Poo')
+},
+]
+
+
