23cb3d09eb7c6c13a9fc2ff9fac185edbd02638c
Caio Nunes <caionunes2801@gmail.com>
2022-07-01 18:38:33 -0300

    fifth commit

---
+++ array-english/script.js
@@ -0,0 +1,18 @@
+var comedy = [{
+  title: 'Deadpool',
+  director: 'Tim Miller',
+  year: 2016,
+}]
+
+var action = [{
+  title: 'Kingsman',
+  director: 'Matthew de Vere Drummond',
+  year: 2014,
+}]
+
+
+var fantasy = [{
+  title: 'Avatar',
+  director: 'James Cameron',
+  year: 2009,
+}]
