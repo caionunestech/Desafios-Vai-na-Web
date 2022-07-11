37f30a69f596a95ffa9976d29f6aeef348dba3c3
Caio Nunes <caionunes2801@gmail.com>
2022-07-11 18:00:26 -0300

    sixth commit

---
+++ lol-desafio/script.js
@@ -0,0 +1,9 @@
+const nickname = document.getElementById('name')
+const password = document.getElementsById('password') 
+const button = document.querySelector('button')
+
+function click() {
+  if (nickname.value = 'E' && password.value.lenght == 6) {
+    button.style.backgroundColor = 'red'
+  }
+}
